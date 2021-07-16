400c2d3df3543d286cba550867127c97041f2e48
Luca <mastrantonio.luca.91@icloud.com>
2021-07-16 16:49:27 +0200

    Dadi ' complete

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -1,3 +1,23 @@
 // **Gioco dei dadi**
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// array dei numeri
+
+var myArray = [1, 2, 3, 4, 5, 6];
+
+// numero random utente
+var numero_utente = myArray[Math.floor(Math.random() * myArray.length)];
+
+// numero random computer
+var numero_computer = myArray[Math.floor(Math.random() * myArray.length)];
+
+// stabilire chi vince
+if (numero_utente > numero_computer) {
+    console.log(numero_utente, numero_computer);
+    alert('giocatore vince');
+}else {
+    console.log(numero_utente, numero_computer);
+    alert('computer vince');
+}
+
